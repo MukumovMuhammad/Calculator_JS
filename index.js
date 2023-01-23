0c6429503a77412c3ede86e5b9fff4424049b820
Muhammad <muhammad_bw_26@mail.ru>
2023-01-23 15:10:59 +0500

    First Commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function  cal(param){
     let num2 = document.getElementById("num2").value;
     let result;
 
-    
+
     if(param ==="+"){
         result = parseFloat(num1) + parseFloat(num2);
         document.getElementById("result").value = Math.round(result);
@@ -25,3 +25,28 @@ function  cal(param){
 
     }
 }
+
+
+
+
+// Arrays
+
+const array = ['a', 'b', 'c', 'd']
+
+// push
+array.push('apple');
+console.log(array);
+
+// pops
+array.pop();
+console.log(array);
+
+// unshift
+array.unshift("apple")
+
+// splice
+// array.splice(start_index, delete_count, Element we wanna add)
+
+array.splice(1,2, "apple" ,"window")
+
+console.log(array)
